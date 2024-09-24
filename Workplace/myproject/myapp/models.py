a4dc5a62afb65e255bb4b690857cfb256b7705de
Raju Tamang <ezycom102@gmail.com>
2024-09-24 08:36:03 +0545

    model person updated

--- Workplace/myproject/myapp/models.py
+++ Workplace/myproject/myapp/models.py
@@ -6,3 +6,6 @@ class Person(models.Model):
     age = models.IntegerField()
     email = models.EmailField()
     phone = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.person_name
