f13849e5f833acb51d1bc78b009227deddc88b5a
Raju Tamang <ezycom102@gmail.com>
2024-10-02 19:43:46 +0545

    Admin.py updated

--- Workplace/myproject/myapp/admin.py
+++ Workplace/myproject/myapp/admin.py
@@ -3,18 +3,15 @@ from django.contrib.auth.admin import User
 from .models import Person, Logger
 
 
-
-
 # Register your models here.
 admin.site.register(Person)
 admin.site.register(Logger)
 
-
 admin.site.unregister(User)
 
 
+from django.contrib.auth.admin import UserAdmin
 
-from django.contrib.auth.admin import UserAdmin 
 @admin.register(User) 
 class NewAdmin(UserAdmin): 
     def get_form(self, request, obj=None, **kwargs): 
